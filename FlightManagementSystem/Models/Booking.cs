f24b173fbed62e94d3f4c38ab070c5d84583a8ba
alzulaikha <alzulaikha1992@gmail.com>
2026-06-23 09:42:31 +0400

    added  properties in booking class

--- FlightManagementSystem/Models/Booking.cs
+++ FlightManagementSystem/Models/Booking.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace FlightManagementSystem.Models
 {
-    internal class Booking
+    public class Booking
     {
+        public int bookingId { get; set; }
+        public int passengerId;
+
+        public int flightId;
+        public int pilotId;
+        public string seatNumber { get; set; }
+        public string bookingDate { get; set; }
+        public string departureTime { get; set; }
+        public decimal totalPrice { get; set; }
+        public string status { get; set; }
     }
 }
