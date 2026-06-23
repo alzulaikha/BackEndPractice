3a22cfa113859959fdfe65ac2c316562f0c50af1
alzulaikha <alzulaikha1992@gmail.com>
2026-06-23 09:29:42 +0400

    added properties in class pilot

--- FlightManagementSystem/Models/Pilot.cs
+++ FlightManagementSystem/Models/Pilot.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace FlightManagementSystem.Models
 {
-    internal class Pilot
+    public class Pilot
     {
+        public int pilotId { get; set; }
+        public string pilotName { get; set; }
+        public string pilotPhone { get; set; }
+
+        public string licenseNumber { get; set; }
+        public int flightHours { get; set; }
+        public bool isAvailable { get; set; }
     }
 }
