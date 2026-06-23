c650659c0795a63e9dd5c72ab34f6cb78c9da178
alzulaikha <alzulaikha1992@gmail.com>
2026-06-23 09:38:30 +0400

    added  properties in flight class

--- FlightManagementSystem/Models/Flight.cs
+++ FlightManagementSystem/Models/Flight.cs
@@ -4,7 +4,18 @@ using System.Text;
 
 namespace FlightManagementSystem.Models
 {
-    internal class Flight
+    public class Flight
     {
+        public int flightId { get; set; }
+        public string flightCode { get; set; }
+
+        public int aircraftId;
+        public int pilotId;
+        public string origin { get; set; }
+        public string destination { get; set; }
+        public string departureTime { get; set; }
+        public decimal ticketPrice { get; set; }
+        public int availableSeats { get; set; }
+        public string status { get; set; }
     }
 }
