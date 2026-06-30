88a6834b4105f761cef91ff73206d68c8bd9dd1e
alzulaikha <alzulaikha1992@gmail.com>
2026-06-30 16:15:37 +0400

    added default constraints "In Progress" to status attribute

--- UniversityManagementSystem/Models/Enrollment.cs
+++ UniversityManagementSystem/Models/Enrollment.cs
@@ -1,18 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace UniversityManagementSystem.Models
 {
     internal class Enrollment
     {
-      
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int enrollmentId { get; set; } //System Generated
+        [ForeignKey("Student")]
         public int studentId { get; set; } //From list/foreign key
+        public Student student { get; set; }
+        [ForeignKey("Course")]
         public int courseId { get; set; } //From list/foreign key
+        public Course course { get; set; }
+
+        [Required]
         public DateTime enrollmentDate { get; set; } //Calculated
+
+        [MaxLength(2)]
         public string finalGrade { get; set; } //Calculated
-        public string status { get; set; } //  Default Value
+        [Required]
+
+        [MaxLength(20)]
+        public string status { get; set; } = "In Progress"; //  Default Value
     }
 }
