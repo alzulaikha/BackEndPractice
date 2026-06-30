42d24ac8a7500f3a9c996c0dbd531b360f68cc87
alzulaikha <alzulaikha1992@gmail.com>
2026-06-30 14:31:21 +0400

    added attribute, data type and source tag legend in enrollment class

--- UniversityManagementSystem/Models/Enrollment.cs
+++ UniversityManagementSystem/Models/Enrollment.cs
@@ -6,6 +6,11 @@ namespace UniversityManagementSystem.Models
 {
     internal class Enrollment
     {
-      
+        public int enrollmentId { get; set; } //System Generated
+        public int studentId { get; set; } //From list/foreign key
+        public int courseId { get; set; } //From list/foreign key
+        public DateTime enrollmentDate { get; set; } //Calculated
+        public string finalGrade { get; set; } //Calculated
+        public string status { get; set; } //  Default Value
     }
 }
