146d6e5082d55f76e083ce75b6cafd10dce70a3e
alzulaikha <alzulaikha1992@gmail.com>
2026-06-30 14:13:38 +0400

    added attribute, data type and source tag legend in Instructor class

--- UniversityManagementSystem/Models/Instructor.cs
+++ UniversityManagementSystem/Models/Instructor.cs
@@ -6,5 +6,17 @@ namespace UniversityManagementSystem.Models
 {
     internal class Instructor
     {
+        public int instructorId { get; set; } //System Generated
+        public string fullName { get; set; } //User Input
+        public string email { get; set; } // User Input
+        public string officeNumber { get; set; } //System Generated
+        public DateTime hireDate { get; set; } // User Input
+        public decimal salary { get; set; } // Calculated
+        public string academicTitle { get; set; } // User Input
+
+
+
+
+
     }
 }
