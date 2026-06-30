a5d6923d6303f133d53d77a5f8ad9b552ebb7971
alzulaikha <alzulaikha1992@gmail.com>
2026-06-30 14:27:50 +0400

    added attribute, data type and source tag legend in course class

--- UniversityManagementSystem/Models/Course.cs
+++ UniversityManagementSystem/Models/Course.cs
@@ -6,5 +6,12 @@ namespace UniversityManagementSystem.Models
 {
     internal class Course
     {
+        public int courseId { get; set; } //System Generated
+        public string courseCode { get; set; } //System Generated
+        public string courseTitle { get; set; } // User Input
+        public int creditHours { get; set; } //Calculated
+        public int departmentId { get; set; }  //From list/foreign key
+        public int instructorId { get; set; }  //From list/foreign key
+        public string semesterOffered { get; set; } // User Input
     }
 }
