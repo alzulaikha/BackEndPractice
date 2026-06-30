37299b2be84fd079e75d20a81f673b43f3900fdd
alzulaikha <alzulaikha1992@gmail.com>
2026-06-30 12:10:09 +0400

    added Attribute and source tags legend in student class

--- UniversityManagementSystem/Models/Enrollment.cs
+++ UniversityManagementSystem/Models/Enrollment.cs
@@ -6,5 +6,6 @@ namespace UniversityManagementSystem.Models
 {
     internal class Enrollment
     {
+      
     }
 }
--- UniversityManagementSystem/Models/Student.cs
+++ UniversityManagementSystem/Models/Student.cs
@@ -6,5 +6,14 @@ namespace UniversityManagementSystem.Models
 {
     internal class Student
     {
+        
+        public int studentId { get; set; } //System Generated
+        public string fullName { get; set; } //User Input
+        public string email { get; set; } // User Input
+        public string phoneNumber { get; set; } // User Input
+        public DateTime dateOfBirth { get; set; } //Calculated
+        public int enrollmentYear { get; set; } // User Input
+        public decimal gpa { get; set; } // Calculated
+
     }
 }
