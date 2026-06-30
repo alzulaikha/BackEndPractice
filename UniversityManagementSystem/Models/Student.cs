d89f15f92e8a1b3beeeb0da1b8638f1e71501954
alzulaikha <alzulaikha1992@gmail.com>
2026-06-30 15:26:52 +0400

    added constraints in student class

--- UniversityManagementSystem/Models/Student.cs
+++ UniversityManagementSystem/Models/Student.cs
@@ -1,18 +1,33 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
-
 namespace UniversityManagementSystem.Models
 {
+    [Index(nameof(email),IsUnique = true)]
     internal class Student
     {
-        
+        [Key]
+        [Required]
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int studentId { get; set; } //System Generated
+        [Required]
+        [MaxLength(100)]
         public string fullName { get; set; } //User Input
+        [Required]
+        [MaxLength(150)]
         public string email { get; set; } // User Input
+        [MaxLength(20)]
         public string phoneNumber { get; set; } // User Input
+        [Required]
         public DateTime dateOfBirth { get; set; } //Calculated
+        [Required]
+        [Range(2000, 2030)]
         public int enrollmentYear { get; set; } // User Input
+        [Range(0.0, 4.0)]
         public decimal gpa { get; set; } // Calculated
 
     }
