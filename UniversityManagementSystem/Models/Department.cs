113ff894e2f6e7ef30a08b8373dcb2b79866bef9
alzulaikha <alzulaikha1992@gmail.com>
2026-06-30 15:44:16 +0400

    added constraints in instructor class

--- UniversityManagementSystem/Models/Department.cs
+++ UniversityManagementSystem/Models/Department.cs
@@ -1,16 +1,29 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace UniversityManagementSystem.Models
 {
+    [Index(nameof(departmentName), IsUnique = true)]
     internal class Department
     {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int departmentId { get; set; } //System Generated
+        [Required]
+        [MaxLength(100)]
         public string departmentName { get; set; } //User Input
+        [MaxLength(50)]
         public string building { get; set; } // User Input
+        [Required]
         public decimal budget { get; set; } //Calculated
+        [ForeignKey("Instructor")]
         public int headInstructorId { get; set; } //From list/foreign key
+        public Instructor Instructor { get; set; }
 
     }
 }
