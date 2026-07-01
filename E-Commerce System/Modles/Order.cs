319352ab03b162ad328eea2eef53ebd953c3b8a3
alzulaikha <alzulaikha1992@gmail.com>
2026-07-01 16:07:45 +0400

    added attribute ,data type and source tag

--- E-Commerce System/Modles/Order.cs
+++ E-Commerce System/Modles/Order.cs
@@ -6,5 +6,13 @@ namespace E_Commerce_System.Modles
 {
     internal class Order
     {
+        public int orderId { get; set; } //System Generated
+        public int userId { get; set; } //From list/forgin key
+        public DateTime orderDate { get; set; } //calculated
+        public decimal totalAmount { get; set; } //calculated
+        public bool status { get; set; } //Default
+        public string shippingAddress { get; set; } // user input
+        public string paymentMethod { get; set; } //user input
+ 
     }
 }
