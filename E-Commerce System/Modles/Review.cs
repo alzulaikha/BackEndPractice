60ea04d1d5d47a56dc9f5402b4bda0c5330ad003
alzulaikha <alzulaikha1992@gmail.com>
2026-07-01 16:11:03 +0400

    added attribute, data type and source tags

--- E-Commerce System/Modles/Review.cs
+++ E-Commerce System/Modles/Review.cs
@@ -6,6 +6,12 @@ namespace E_Commerce_System.Modles
 {
     internal class Review
     {
-        
+        public int reviewId { get; set; } //System Generated
+        public int userId { get; set; } //From list/forgin key
+        public int productId { get; set; } //From list/forgin key
+        public int rating { get; set; } //calculated
+        public string comment { get; set; } //user input
+        public DateTime reviewDate { get; set; } //calculated
+
     }
 }
