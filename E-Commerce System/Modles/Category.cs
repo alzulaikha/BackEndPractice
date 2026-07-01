5a8ab65a80abd4c1f8bbb21a657a878a607e011a
alzulaikha <alzulaikha1992@gmail.com>
2026-07-01 15:57:25 +0400

    added attribute , data type and  source tags

--- E-Commerce System/Modles/Category.cs
+++ E-Commerce System/Modles/Category.cs
@@ -6,5 +6,10 @@ namespace E_Commerce_System.Modles
 {
     internal class Category
     {
+        public int categoryId { get; set; } //System Generated
+        public string categoryName { get; set; } //User input
+        public string description { get; set; } // User input 
+        public string imageUrl { get; set; } //User input
+  
     }
 }
