cab25575f96b6835f6590d2265ce2171a4607142
alzulaikha <alzulaikha1992@gmail.com>
2026-07-01 16:03:20 +0400

    added attribute , data type and source tags in Product class

--- E-Commerce System/Modles/Product.cs
+++ E-Commerce System/Modles/Product.cs
@@ -6,5 +6,14 @@ namespace E_Commerce_System.Modles
 {
     internal class Product
     {
+        public int productId { get; set; } //System Generated
+        public string productName { get; set; } //User input
+        public string description { get; set; } // User input 
+        public decimal price { get; set; } //User input
+        public int stockQuantity { get; set; }//calculated
+        public string imageUrl { get; set; } // user input
+        public int categoryId { get; set; } //  From List/ forgin key
+        public DateTime createdAt { get; set; } //calculated
+        public bool isAvailable { get; set; } //Default
     }
 }
