c42d25d2256bc635f5a48ab5ccd2d395829f3f08
alzulaikha <alzulaikha1992@gmail.com>
2026-07-05 14:55:12 +0400

    added constraints to Category class

--- E-Commerce System/Modles/Category.cs
+++ E-Commerce System/Modles/Category.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace E_Commerce_System.Modles
 {
+    [Index(nameof(categoryName), IsUnique = true)]
     internal class Category
     {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int categoryId { get; set; } //System Generated
-        public string categoryName { get; set; } //User input
-        public string description { get; set; } // User input 
-        public string imageUrl { get; set; } //User input
-  
+        [Required]
+        [MaxLength(100)]
+        public string categoryName { get; set; } //User input 
+        [MaxLength(500)]
+        public string? description { get; set; } // User input /Optional
+        [MaxLength(300)]
+        public string? imageUrl { get; set; } //User input  /Optional
+
     }
 }
+
+[Index(nameof(courseCode), IsUnique = true)]
