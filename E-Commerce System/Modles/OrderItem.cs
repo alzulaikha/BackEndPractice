9ffbb3d30076a194c0665e0a6bc4a0c80065a81c
alzulaikha <alzulaikha1992@gmail.com>
2026-07-06 16:28:20 +0400

    Added attribute , data type and constraints

--- E-Commerce System/Modles/OrderItem.cs
+++ E-Commerce System/Modles/OrderItem.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace E_Commerce_System.Modles
 {
     internal class OrderItem
     {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int orderItemId { get; set; } // system generated
+
+
+        //  relationship carries the attribute 'quantity' 
+        [Required]
+        [Range(1, 999)]
+        public int quantity { get; set; }// user input
+
+
+
+
+ 
+
+
     }
 }
