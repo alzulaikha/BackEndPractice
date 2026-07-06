2f0c8553feab150e48f302548e69b5804270543b
alzulaikha <alzulaikha1992@gmail.com>
2026-07-06 16:44:46 +0400

    Add DbSet properties for all entities in EcommerceContext

---
+++ E-Commerce System/EcommerceContext.cs
@@ -0,0 +1,17 @@
+﻿using E_Commerce_System.Modles;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_Commerce_System
+{
+    public class EcommerceContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+    }
+}
